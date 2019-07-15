34df4bf8e5e0ab878fe344e271792aa74ea24a96
lauren-hering <lauren.hering@comcast.net>
2019-07-15 16:40:33 -0500

    initial commit

--- PhotoPractice/ViewController.swift
+++ PhotoPractice/ViewController.swift
@@ -8,13 +8,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    var imagePicker = UIImagePickerController()
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        imagePicker.delegate = self
+        
     }
 
-
+    @IBAction func takeSelfieTapped(_ sender: UIButton) {
+        imagePicker.sourceType = .camera
+        
+        present(imagePicker, animated : true, completion : nil)
+    }
+    
+    @IBAction func libraryButtonTapped(_ sender: UIButton) {
+    
+        imagePicker.sourceType = .photoLibrary
+        
+        present(imagePicker, animated: true, completion: nil)
+    }
+  
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {imageView.image = selectedImage
+            
+        }
+    imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
 }
 
